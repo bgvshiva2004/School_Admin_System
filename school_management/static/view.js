1c13e056012223b56929ea10bb401ba4a7a12be4
B.G.V.Shiva <118362268+bgvshiva2004@users.noreply.github.com>
2023-11-16 15:18:11 +0530

    Update view.js

--- school_management/static/view.js
+++ school_management/static/view.js
@@ -1,67 +1,88 @@
 var side_bar = document.querySelector(".side-bar");
-    var ham = document.getElementById("menus");
-    var close = document.getElementById("close");
-
-    ham.addEventListener("click", ()=>{
-        side_bar.classList.toggle("show-side");
-    });
-
-    close.addEventListener("click", ()=>{
-      side_bar.classList.toggle("show-side");
-    });
-
-    var nav1 = document.querySelector(".navbar1");
-    var val;
-    window.onscroll = function(){
-        if(document.documentElement.scrollTop > 20){
-            nav1.classList.add("sticky");
-        }
-        else{
-            nav1.classList.remove("sticky");
-        }
+var ham = document.getElementById("menus");
+var close = document.getElementById("close");
+ham.addEventListener("click", ()=>{
+    side_bar.classList.toggle("show-side");
+});
+close.addEventListener("click", ()=>{
+  side_bar.classList.toggle("show-side");
+});
+var nav1 = document.querySelector(".navbar1");
+var val;
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav1.classList.add("sticky");
     }
+    else{
+        nav1.classList.remove("sticky");
+    }
+}
 
 
 const classwise_student_list = document.getElementById('classwise_student_list');
-const classwise_list= document.getElementById('classwise_list');
-const past_finance = document.getElementById('past_finance');
-const finance_list = document.getElementById('finance_list');
-const fee = document.getElementById('fee');
-const fee_date = document.getElementById('fee_date');
-const expenditure = document.getElementById('expenditure');
-const expenditure_date = document.getElementById('expenditure_date');
+        const classwise_list= document.getElementById('classwise_list');
+        const past_finance = document.getElementById('past_finance');
+        const finance_list = document.getElementById('finance_list');
+        const fee = document.getElementById('fee');
+        const fee_date = document.getElementById('fee_date');
+        const expenditure = document.getElementById('expenditure');
+        const expenditure_date = document.getElementById('expenditure_date');
 
 
-const student_complete_details=document.getElementById('student_complete_details');
-const student_details_form = document.getElementById('student_details_form');
+        const student_complete_details=document.getElementById('student_complete_details');
+        const student_details_form = document.getElementById('student_details_form');
 
-const employee_details =document.getElementById('employee_details');
-const employee_details_form=document.getElementById('employee_details_form');
+        const employee_details =document.getElementById('employee_details');
+        const employee_details_form=document.getElementById('employee_details_form');
 
-const classwise_table = document.getElementById("classwise_table");
-const student_bio = document.getElementById("student_bio");
-const employee_bio = document.getElementById("employee_bio");
-const fee_record_table =document.getElementById("fee_record_table");
-const exp_record_table = document.getElementById("exp_record_table");
+        classwise_student_list.addEventListener('click',function(){
+          fee_date.style.display = 'none';
+          expenditure_date.style.display = 'none';
+            student_details_form.style.display = 'none';
+            employee_details_form.style.display = 'none';
+            finance_list.style.display = 'none';
+            classwise_list.style.display = 'block';
+        });
 
-function hideAll(){
-    classwise_list.style.display = 'none';
-    student_details_form.style.display = 'none';
-    employee_details_form.style.display = 'none';
-    fee_date.style.display = 'none';
-    expenditure_date.style.display = 'none';
-    classwise_table.style.display = 'none';
-    student_bio.style.display = 'none';
-    employee_bio.style.display = 'none';
-    fee_record_table.style.display = 'none';
-    exp_record_table.style.display = 'none';
-}
+        student_complete_details.addEventListener('click',function(){
+          fee_date.style.display = 'none';
+          expenditure_date.style.display = 'none';
+            classwise_list.style.display = 'none';
+            employee_details_form.style.display = 'none';
+            finance_list.style.display = 'none';
+            student_details_form.style.display = 'block';
+        });
 
-classwise_student_list.addEventListener('click',function(){
-    hideAll();
-    classwise_list.style.display = 'block';
-    classwise_table.style.display = 'block';
-});
+        employee_details.addEventListener('click',function(){
+          fee_date.style.display = 'none';
+          expenditure_date.style.display = 'none';
+            classwise_list.style.display = 'none';
+            student_details_form.style.display = 'none';
+            finance_list.style.display = 'none';
+            employee_details_form.style.display = 'block';
+        });
+
+        past_finance.addEventListener('click',function(){
+          classwise_list.style.display = 'none';
+          student_details_form.style.display = 'none';
+          employee_details_form.style.display = 'none';
+          fee_date.style.display = 'none';
+          expenditure_date.style.display = 'none';
+          finance_list.style.display = 'block';
+        });
+
+        fee.addEventListener('click',function(){
+          expenditure_date.style.display = 'none';
+          fee_date.style.display = 'block';
+        });
+        expenditure.addEventListener('click',function(){
+          fee_date.style.display = 'none';
+          expenditure_date.style.display = 'block';
+        });
+
+
+
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
